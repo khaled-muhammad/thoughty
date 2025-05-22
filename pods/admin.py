c35652f4c73c4879a9a42c461a0e25e54b780e3b
khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-05-22 20:13:40 +0300

    simple cleaning.

--- pods/admin.py
+++ pods/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
 from .models import Pod, PodStageHistory, Tag
 
-# Register your models here.
+class PodStageHistoryInline(admin.TabularInline):
+    model = PodStageHistory
+    extra = 0
+    readonly_fields = ['version', 'content', 'created_at', 'timestamp']
+    can_delete = False
 
-admin.site.register(Pod)
-admin.site.register(PodStageHistory)
-admin.site.register(Tag)
+@admin.register(Pod)
+class PodAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'stage', 'version', 'is_public', 'created_at']
+    list_filter = ['stage', 'is_public', 'created_at']
+    search_fields = ['title', 'content', 'user__username']
+    readonly_fields = ['version', 'created_at', 'timestamp']
+    filter_horizontal = ['tags']
+    inlines = [PodStageHistoryInline]
+
+@admin.register(PodStageHistory)
+class PodStageHistoryAdmin(admin.ModelAdmin):
+    list_display = ['pod', 'version', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['pod__title', 'content']
+    readonly_fields = ['created_at', 'timestamp']
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
